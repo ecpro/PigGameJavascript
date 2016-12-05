5cb1575d3c0c9e8ed5789a2891ec946be7686cce
Piyush Ravi <mails4ipiyush@gmail.com>
2016-12-05 17:18:39 +0530

    refactor code to follow DRY principle

--- app.js
+++ app.js
@@ -9,33 +9,33 @@ GAME RULES:
 
 */
 
-$(function(){
+$(function () {
 
-    //start game and initialize players 
+    var current, activePlayer, diceVal, players;
 
-    var current,activePlayer;
-    current = 0;
-    activePlayer = Math.floor(Math.random() * 2);
+    // initiallize variables and start game
+    init();
     
-    $(".player-"+activePlayer+"-panel").addClass("active");
+    // new game button 
     
-    // roll dice event
-    
-    $(".btn-roll").on("click", function(event){
-        event.preventDefault();
-        var diceVal,diceImage,dice;
-        var totalDelay = 0;
-        diceVal = Math.floor(Math.random() * 6) + 1;
-        console.log("dice value : " + diceVal);
-        diceImage = "dice-" + diceVal + ".png";
-        //console.log("image " + diceImage);
+    $(".btn-new").on("click", function (event) {
+        $(".player-" + activePlayer + "-panel").toggleClass("active");
+        init();
+    });
+
+
+    // rollDice
+
+    function rollDice() {
+        var diceImage, dice, totalDelay = 0;
+
         dice = $(".dice");
-        
-        // dice animation
-        
+
+        // 1. rotate dice animation
+
         function rotateDice() {
-            for(var x = 1; x < 7; x++) {
-                
+            for (var x = 1; x < 7; x++) {
+
                 function rotate() {
                     var random = Math.floor(Math.random() * 6) + 1;
                     var randomDiceImage = "dice-" + random + ".png";
@@ -43,80 +43,93 @@ $(function(){
                 }
                 totalDelay += 10 * x;
                 setTimeout(rotate, 10 * x);
-                                
+
             }
-            
+
         }
-        
+
         rotateDice();
-        
-        // update active players current score 
-        
-        setTimeout(function (){
+
+        // 2. get a random dice value
+        diceVal = Math.floor(Math.random() * 6) + 1;
+
+        // 3. set the dice image corresponding to value generated in step 2
+        diceImage = "dice-" + diceVal + ".png";
+
+        //console.log("dice value : " + diceVal);
+        //console.log("image " + diceImage);
+
+        // 4. update the current score and activePlayer score
+
+
+        // setTimeout to wait untill rotateDice finishes
+        setTimeout(function () {
             dice.attr("src", diceImage);
-           
-            if(diceVal !== 1) {
+
+            // if diceVal === 1 then set the current to activePlayer's score and togglePlayer 
+
+            if (diceVal !== 1) {
+                players[activePlayer] += diceVal;
+                console.log(activePlayer + " " + players[activePlayer]);
                 current += diceVal;
                 console.log("new current" + current);
-                $("#current-"+activePlayer).text(current);
-            }
-            else {
-                var curr = parseInt($("#score-"+activePlayer).text());
-                current = curr + current;
-                $("#score-"+activePlayer).text(current);
-                current = 0;
-                $("#current-"+activePlayer).text(current);
+                $("#current-" + activePlayer).text(current);
+
+                if (players[activePlayer] >= 100) {
+                    $("#name-" + activePlayer).text("Winner");
+                    $(".player-" + activePlayer + "-panel").addClass("winner");
+                    togglePlayer();
+                    console.log(this);
+                    $(".btn-roll").off("click");
+                    $(".btn-hold").off("click");
+                }
+
+            } else {
                 togglePlayer();
             }
-            
-        }, totalDelay);
-        
-    });
-    
-    // hold button function implementation
-    
-    $(".btn-hold").on("click", function(event) {
-        event.preventDefault();
-        var temp = parseInt($("#score-" + activePlayer).text());
-        current += temp;
-        $("#score-"+ activePlayer).text(current);
-        current = 0;
-        $("#current-"+activePlayer).text(current);
-        togglePlayer();
-    });
-    
-    
-    $(".btn-new").on("click", function(event) {
-       resetGame(); 
-    });
-    
-    // toggle players 
-    
-        function togglePlayer() {
-            if(activePlayer === 0) {
-                activePlayer = 1;
-            }
-            else {
-                activePlayer = 0;
-            }
-         $(".player-0-panel").toggleClass("active");
-         $(".player-1-panel").toggleClass("active");
 
-            
+        }, totalDelay);
+
+    }
+
+
+
+    // togglePlayer
+
+    function togglePlayer() {
+        $("#score-" + activePlayer).text(players[activePlayer]);
+        current = 0;
+        $("#current-" + activePlayer).text(current);
+
+        if (activePlayer === 0) {
+            activePlayer = 1;
+        } else {
+            activePlayer = 0;
         }
-       
-    // reset game 
-    
-    function resetGame() {
+        $(".player-0-panel").toggleClass("active");
+        $(".player-1-panel").toggleClass("active");
+
+
+    }
+
+    // init game
+
+    function init() {
         $("#score-0").text(0);
         $("#score-1").text(0);
-        $("#current-0").text("0");
-        $("#current-1").text("0");
-        $(".player-"+activePlayer+"-panel").toggleClass("active");
+        $("#current-0").text(0);
+        $("#current-1").text(0);
+        $("#name-0").text("Player 1");
+        $("#name-1").text("Player 2");
         activePlayer = Math.floor(Math.random() * 2);
-        $(".player-"+activePlayer+"-panel").toggleClass("active");
+        $(".player-" + activePlayer + "-panel").addClass("active");
+        $(".player-0-panel").removeClass("winner");
+        $(".player-1-panel").removeClass("winner");
+        players = [0, 0];
+        current = 0;
+        $(".btn-hold").bind("click", togglePlayer);
+        $(".btn-roll").bind("click", rollDice);
+    }
 
-}
-    
-    
+
 });
